cacca465cadcb109aee8064620c655db5d392282
Diggory Blake <diggsey@googlemail.com>
2015-10-11 17:13:03 +0100

    Make failures more obvious

--- tests/cli.bat
+++ tests/cli.bat
@@ -5,30 +5,30 @@ echo ^> Running CLI tests...
 set MR="%~dp0\..\target\release\multirust-rs.exe"
 
 echo ^> Testing --help
-%MR% --help || exit /b 1
+%MR% --help || (echo FAILED && exit /b 1)
 
 echo ^> Testing install
-%MR% install -a || exit /b 1
+%MR% install -a || (echo FAILED && exit /b 1)
 
 echo ^> Updating PATH
 set PATH=%USERPROFILE%\.multirust\bin;%PATH%
 
 echo ^> Testing default
-multirust default nightly || exit /b 1
+multirust default nightly || (echo FAILED && exit /b 1)
 
 echo ^> Testing rustc
-rustc --multirust || exit /b 1
+rustc --multirust || (echo FAILED && exit /b 1)
 
 echo ^> Testing cargo
-cargo --multirust || exit /b 1
+cargo --multirust || (echo FAILED && exit /b 1)
 
 echo ^> Testing override
-multirust override i686-msvc-stable || exit /b 1
+multirust override i686-msvc-stable || (echo FAILED && exit /b 1)
 
 echo ^> Testing update
-multirust update || exit /b 1
+multirust update || (echo FAILED && exit /b 1)
 
 echo ^> Testing uninstall
-multirust uninstall -y || exit /b 1
+multirust uninstall -y || (echo FAILED && exit /b 1)
 
 echo ^> Finished
