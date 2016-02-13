3b4cdaf4475513b4ac44830a854e548615051387
llogiq <bogusandre@gmail.com>
2016-02-13 10:48:51 +0100

    fixed some clippy warnings

--- src/errors.rs
+++ src/errors.rs
@@ -133,7 +133,7 @@ impl Display for Error {
                 write!(f,
                        "infinite recursion detected: the command may not exist for this toolchain")
             }
-            Custom { id: _, ref desc } => write!(f, "{}", desc),
+            Custom { ref desc, .. } => write!(f, "{}", desc),
         }
     }
 }
