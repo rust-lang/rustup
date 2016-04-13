32269fec70bce762f73498617a2c68575c0af42f
Jorge Aparicio <japaricious@gmail.com>
2016-04-13 17:35:58 -0500

    dist: pick the right host triple on ARMv7 devices
    instead of using the constants in `std::env::consts` (which can't
    differentiate between ARMv6 and ARMv7 targets) use the TARGET env
    variable set by Cargo which exactly matches the triple we want to use!
    
    closes #308

---
+++ src/multirust-dist/build.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let target = env::var("TARGET").unwrap();
+
+    File::create(out_dir.join("target.txt")).unwrap().write_all(target.as_bytes()).unwrap();
+}
