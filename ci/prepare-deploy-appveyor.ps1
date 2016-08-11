8a28161955ab91728cfa3b7ac1d1011e621f4ada
Patrick Reisert <kpreisert@gmail.com>
2016-08-11 11:58:48 +0200

    Build MSI on AppVeyor

--- ci/prepare-deploy-appveyor.ps1
+++ ci/prepare-deploy-appveyor.ps1
@@ -6,6 +6,11 @@ if ($env:APPVEYOR_REPO_BRANCH -eq "auto") {
    exit 0
 }
 
+# Don't do anything for MSI (yet)
+if ($env:BUILD_MSI) {
+   exit 0
+}
+
 # Copy rustup-init to rustup-setup for backwards compatibility
 cp target\${env:TARGET}\release\rustup-init.exe target\${env:TARGET}release\rustup-setup.exe
 
