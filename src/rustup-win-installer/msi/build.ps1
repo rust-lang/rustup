c426cae2988716606d4deb10a1242b9dcc44a713
Patrick Reisert <kpreisert@gmail.com>
2016-08-13 14:34:28 +0200

    Use Cargo workspace and pass target specification to WiX

--- src/rustup-win-installer/msi/build.ps1
+++ src/rustup-win-installer/msi/build.ps1
@@ -1,3 +1,8 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $Target
+)
+
 $manifest = cargo read-manifest --manifest-path ..\..\..\Cargo.toml | ConvertFrom-Json
 $version = $manifest.version.Split(".")
 $env:CFG_VER_MAJOR = $version[0]
@@ -7,8 +12,8 @@ $env:CFG_VER_PATCH = $version[2]
 foreach($file in Get-ChildItem *.wxs) {
     $in = $file.Name
     $out = $($file.Name.Replace(".wxs",".wixobj"))
-    &"$($env:WIX)bin\candle.exe" -nologo -arch x86 -ext WixUIExtension -ext WixUtilExtension -out "target\$out" $in
-    if ($LASTEXITCODE -ne 0) { exit }
+    &"$($env:WIX)bin\candle.exe" -nologo -arch x86 "-dTARGET=$Target" -ext WixUIExtension -ext WixUtilExtension -out "target\$out" $in
+    if ($LASTEXITCODE -ne 0) { exit 1 }
 }
 
 # ICE57 wrongly complains about per-machine data in per-user install, because it doesn't know that INSTALLLOCATION is in per-user directory
