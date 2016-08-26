063dddec80733178746ede088783278572e71921
Patrick Reisert <kpreisert@gmail.com>
2016-08-26 12:26:21 +0200

    Deploy MSI build artifacts

--- ci/prepare-deploy-appveyor.ps1
+++ ci/prepare-deploy-appveyor.ps1
@@ -6,11 +6,6 @@ if ($env:APPVEYOR_REPO_BRANCH -eq "auto") {
    exit 0
 }
 
-# Don't do anything for MSI (yet)
-if ($env:BUILD_MSI) {
-   exit 0
-}
-
 # Copy rustup-init to rustup-setup for backwards compatibility
 cp target\${env:TARGET}\release\rustup-init.exe target\${env:TARGET}release\rustup-setup.exe
 
@@ -20,9 +15,16 @@ Get-FileHash .\target\${env:TARGET}\release\* | ForEach-Object {[io.file]::Write
 # Prepare bins for upload
 $dest = "dist\$env:TARGET"
 md -Force "$dest"
-cp target\${env:TARGET}\release\rustup-init.exe "$dest/"
-cp target\${env:TARGET}\release\rustup-init.exe.sha256 "$dest/"
-cp target\${env:TARGET}\release\rustup-setup.exe "$dest/"
-cp target\${env:TARGET}\release\rustup-setup.exe.sha256 "$dest/"
+if ($env:BUILD_MSI) {
+    # MSI only needs the actual MSI installer and a hash of the embedded rustup.exe for the self update check
+    # This hash is different from rustup-init.exe.sha256 because it is built with the `msi-installed` feature flag
+    cp target\${env:TARGET}\release\rustup-init.exe.sha256 "$dest\rustup-msi.exe.sha256"
+    cp src\rustup-win-installer\msi\target\rustup.msi "$dest\"
+} else {
+    cp target\${env:TARGET}\release\rustup-init.exe "$dest\"
+    cp target\${env:TARGET}\release\rustup-init.exe.sha256 "$dest\"
+    cp target\${env:TARGET}\release\rustup-setup.exe "$dest\"
+    cp target\${env:TARGET}\release\rustup-setup.exe.sha256 "$dest\"
+}
 
 ls "$dest"
