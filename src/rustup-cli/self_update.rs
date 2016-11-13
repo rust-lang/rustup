b99acc6c41b4b8ec7bae3e90aaec7013d7dc4af2
Ryan Havar <rhavar@protonmail.com>
2016-11-13 17:38:45 -0600

    Remove trailing . from instructions
    Even though the . makes sense grammatically, it can confuse users.

--- src/rustup-cli/self_update.rs
+++ src/rustup-cli/self_update.rs
@@ -118,7 +118,7 @@ To get started you need Cargo's bin directory in your `PATH`
 environment variable. Next time you log in this will be done
 automatically.
 
-To configure your current shell run `source {cargo_home}/env`.
+To configure your current shell run `source {cargo_home}/env`
 "
     };
 }
@@ -141,7 +141,7 @@ r"# Rust is installed now. Great!
 To get started you need Cargo's bin directory in your `PATH`
 environment variable.
 
-To configure your current shell run `source {cargo_home}/env`.
+To configure your current shell run `source {cargo_home}/env`
 "
     };
 }
