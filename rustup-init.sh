d2ffd6152f2b1203e7e828df33d4973616510075
Wang Xuerui <idontknw.wang@gmail.com>
2016-11-21 16:02:05 +0800

    rustup-init.sh: add endianness detection

--- rustup-init.sh
+++ rustup-init.sh
@@ -122,6 +122,25 @@ get_bitness() {
     fi
 }
 
+get_endianness() {
+    local cputype=$1
+    local suffix_eb=$2
+    local suffix_el=$3
+
+    # detect endianness without od/hexdump, like get_bitness() does.
+    need_cmd head
+    need_cmd tail
+
+    local _current_exe_endianness="$(head -c 6 /proc/self/exe | tail -c 1)"
+    if [ "$_current_exe_endianness" = "$(printf '\001')" ]; then
+        echo "${cputype}${suffix_el}"
+    elif [ "$_current_exe_endianness" = "$(printf '\002')" ]; then
+        echo "${cputype}${suffix_eb}"
+    else
+        err "unknown platform endianness"
+    fi
+}
+
 get_architecture() {
 
     local _ostype="$(uname -s)"
