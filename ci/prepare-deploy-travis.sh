6c58f1d30f34eb612daf76ed10ae8cac9e0fa572
Matt Ickstadt <mattico8@gmail.com>
2017-05-12 14:36:32 -0500

    Re-enable doc uploading on travis rust-lang/rust#32532 is fixed, so this might work now. Let's find out.

--- ci/prepare-deploy-travis.sh
+++ ci/prepare-deploy-travis.sh
@@ -12,15 +12,13 @@ fi
 
 # Upload docs
 if [[ "$TARGET" == "x86_64-unknown-linux-gnu" && "$TRAVIS_BRANCH" == "stable" ]]; then
-    # FIXME rust-lang/rust#32532
-    printf "not uploading docs"
-    #git config --global credential.helper store;
-    #echo "https://${TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials;
-    #cargo doc --release;
-    #echo '<meta http-equiv=refresh content=0;url=multirust/index.html>' > target/doc/index.html;
-    #sudo pip install ghp-import;
-    #ghp-import -n target/doc;
-    #git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages;
+    git config --global credential.helper store;
+    echo "https://${TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials;
+    cargo doc --release;
+    echo '<meta http-equiv=refresh content=0;url=multirust/index.html>' > target/doc/index.html;
+    sudo pip install ghp-import;
+    ghp-import -n target/doc;
+    git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages;
 fi;
 
 # Copy rustup-init to rustup-setup for backwards compatibility
