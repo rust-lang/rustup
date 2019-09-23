75b46375cb3699a7551b5fb071393f910d5a174a
Lzu Tao <taolzu@gmail.com>
2019-09-23 14:31:18 +0000

    Replace markdown with pulldown-cmark

--- src/cli/markdown.rs
+++ src/cli/markdown.rs
@@ -1,9 +1,9 @@
 // Write Markdown to the terminal
 
 use crate::term2::{color, Attr, Terminal};
-use markdown::tokenize;
-use markdown::{Block, ListItem, Span};
 use std::io;
+
+use pulldown_cmark::{Event, Tag};
 
 // Handles the wrapping of text written to the console
 struct LineWrapper<'a, T: Terminal> {
@@ -85,6 +85,7 @@ impl<'a, T: Terminal + 'a> LineWrapper<'a, T> {
 
 // Handles the formatting of text
 struct LineFormatter<'a, T: Terminal + io::Write> {
+    is_code_block: bool,
     wrapper: LineWrapper<'a, T>,
     attrs: Vec<Attr>,
 }
@@ -92,6 +93,7 @@ struct LineFormatter<'a, T: Terminal + io::Write> {
 impl<'a, T: Terminal + io::Write + 'a> LineFormatter<'a, T> {
     fn new(w: &'a mut T, indent: u32, margin: u32) -> Self {
         LineFormatter {
+            is_code_block: false,
             wrapper: LineWrapper::new(w, indent, margin),
             attrs: Vec::new(),
         }
@@ -107,78 +109,114 @@ impl<'a, T: Terminal + io::Write + 'a> LineFormatter<'a, T> {
             let _ = self.wrapper.w.attr(*attr);
         }
     }
-    fn do_spans(&mut self, spans: Vec<Span>) {
-        for span in spans {
-            match span {
-                Span::Break => {}
-                Span::Text(text) => {
-                    self.wrapper.write_span(&text);
-                }
-                Span::Code(code) => {
-                    self.push_attr(Attr::Bold);
-                    self.wrapper.write_word(&code);
-                    self.pop_attr();
-                }
-                Span::Emphasis(spans) => {
-                    self.push_attr(Attr::ForegroundColor(color::BRIGHT_RED));
-                    self.do_spans(spans);
-                    self.pop_attr();
-                }
-                _ => {}
+
+    fn start_tag(&mut self, tag: Tag<'a>) {
+        match tag {
+            Tag::Paragraph => {
+                self.wrapper.write_line();
             }
-        }
-    }
-    fn do_block(&mut self, b: Block) {
-        match b {
-            Block::Header(spans, _) => {
+            Tag::Heading(_level) => {
                 self.push_attr(Attr::Bold);
                 self.wrapper.write_line();
-                self.do_spans(spans);
+            }
+            Tag::Table(_alignments) => {}
+            Tag::TableHead => {}
+            Tag::TableRow => {}
+            Tag::TableCell => {}
+            Tag::BlockQuote => {}
+            Tag::CodeBlock(_lang) => {
+                self.wrapper.write_line();
+                self.wrapper.indent += 2;
+                self.is_code_block = true;
+            }
+            Tag::List(_) => {
+                self.wrapper.write_line();
+                self.wrapper.indent += 2;
+            }
+            Tag::Item => {
+                self.wrapper.write_line();
+            }
+            Tag::Emphasis => {
+                self.push_attr(Attr::ForegroundColor(color::BRIGHT_RED));
+            }
+            Tag::Strong => {}
+            Tag::Strikethrough => {}
+            Tag::Link(_link_type, _dest, _title) => {}
+            Tag::Image(_link_type, _dest, _title) => {}
+            Tag::FootnoteDefinition(_name) => {}
+        }
+    }
+
+    fn end_tag(&mut self, tag: Tag<'a>) {
+        match tag {
+            Tag::Paragraph => {
+                self.wrapper.write_line();
+            }
+            Tag::Heading(_level) => {
                 self.wrapper.write_line();
                 self.pop_attr();
             }
-            Block::CodeBlock(code) => {
-                self.wrapper.write_line();
-                self.wrapper.indent += 2;
-                for line in code.lines() {
-                    // Don't word-wrap code lines
-                    self.wrapper.write_word(line);
-                    self.wrapper.write_line();
-                }
+            Tag::Table(_) => {}
+            Tag::TableHead => {}
+            Tag::TableRow => {}
+            Tag::TableCell => {}
+            Tag::BlockQuote => {}
+            Tag::CodeBlock(_) => {
+                self.is_code_block = false;
                 self.wrapper.indent -= 2;
             }
-            Block::Paragraph(spans) => {
-                self.wrapper.write_line();
-                self.do_spans(spans);
+            Tag::List(_) => {
+                self.wrapper.indent -= 2;
                 self.wrapper.write_line();
             }
-            Block::UnorderedList(items) => {
-                self.wrapper.write_line();
-                for item in items {
-                    self.wrapper.indent += 2;
-                    match item {
-                        ListItem::Simple(spans) => {
-                            self.do_spans(spans);
-                        }
-                        ListItem::Paragraph(blocks) => {
-                            for block in blocks {
-                                self.do_block(block);
-                            }
-                        }
-                    }
-                    self.wrapper.write_line();
-                    self.wrapper.indent -= 2;
+            Tag::Item => {}
+            Tag::Emphasis => {
+                self.pop_attr();
+            }
+            Tag::Strong => {}
+            Tag::Strikethrough => {}
+            Tag::Link(_, _, _) => {}
+            Tag::Image(_, _, _) => {} // shouldn't happen, handled in start
+            Tag::FootnoteDefinition(_) => {}
+        }
+    }
+
+    fn process_event(&mut self, event: Event<'a>) {
+        use self::Event::*;
+        match event {
+            Start(tag) => self.start_tag(tag),
+            End(tag) => self.end_tag(tag),
+            Text(text) => {
+                if self.is_code_block {
+                    self.wrapper.write_word(&text);
+                } else {
+                    self.wrapper.write_span(&text);
                 }
             }
-            _ => {}
+            Code(code) => {
+                self.push_attr(Attr::Bold);
+                self.wrapper.write_word(&code);
+                self.pop_attr();
+            }
+            Html(_html) => {}
+            SoftBreak => {
+                self.wrapper.write_line();
+            }
+            HardBreak => {
+                self.wrapper.write_line();
+            }
+            Rule => {}
+            FootnoteReference(_name) => {}
+            TaskListMarker(true) => {}
+            TaskListMarker(false) => {}
         }
     }
 }
 
 pub fn md<'a, S: AsRef<str>, T: Terminal + io::Write + 'a>(t: &'a mut T, content: S) {
     let mut f = LineFormatter::new(t, 0, 79);
-    let blocks = tokenize(content.as_ref());
-    for b in blocks {
-        f.do_block(b);
+    let parser = pulldown_cmark::Parser::new(content.as_ref());
+    for event in parser {
+        f.process_event(event);
     }
 }
