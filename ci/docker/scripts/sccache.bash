586cac0d0359d1cd315db1c01a8cd1f715806439
Lzu Tao <taolzu@gmail.com>
2019-09-27 15:19:43 +0700

    build: Upgrade sccache to 0.2.11

--- ci/docker/scripts/sccache.bash
+++ ci/docker/scripts/sccache.bash
@@ -1,22 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -xe
 
-VERSION=0.2.8
+VERSION=0.2.11
 TARGET=x86_64-unknown-linux-musl
 BIN_DIR=/usr/local/bin
 TEMP_DIR=$(mktemp -d)
 
+cd "${TEMP_DIR}"
 mkdir -p "${BIN_DIR}"
 
-curl \
-  -o "${TEMP_DIR}/sccache.tar.gz" \
-  -sSL \
-  "https://github.com/mozilla/sccache/releases/download/${VERSION}/sccache-${VERSION}-${TARGET}.tar.gz"
-
-tar \
-  -xzf "${TEMP_DIR}/sccache.tar.gz" \
-  -C "${BIN_DIR}" \
-  --strip-components 1 \
-  "sccache-${VERSION}-${TARGET}/sccache"
-
+curl -sSL "https://github.com/mozilla/sccache/releases/download/${VERSION}/sccache-${VERSION}-${TARGET}.tar.gz" | \
+tar -xzf - --strip-components 1
+cp sccache "${BIN_DIR}/sccache"
 chmod +x "${BIN_DIR}/sccache"
