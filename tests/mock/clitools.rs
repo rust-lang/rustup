a93284b929d2d6716f8bedebcbf3658b09763e9c
Robert Collins <robertc@robertcollins.net>
2020-06-12 09:39:44 +1200

    Fix tests that trigger clap colourisation
    In-process tests have a tty and clap doesn't honour our process()
    abstraction; fortunately it does consult TERM, which is good enough.

--- tests/mock/clitools.rs
+++ tests/mock/clitools.rs
@@ -92,6 +92,10 @@ pub fn setup(s: Scenario, f: &dyn Fn(&mut Config)) {
     env::remove_var("RUSTUP_TOOLCHAIN");
     env::remove_var("SHELL");
     env::remove_var("ZDOTDIR");
+    // clap does it's own terminal colour probing, and that isn't
+    // trait-controllable, but it does honour the terminal. To avoid testing
+    // claps code, lie about whatever terminal this process was started under.
+    env::set_var("TERM", "dumb");
 
     match env::var("RUSTUP_BACKTRACE") {
         Ok(val) => env::set_var("RUST_BACKTRACE", val),
