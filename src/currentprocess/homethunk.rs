0b8f815c4eff435dd77362de927e6ba53fe42d87
Robert Collins <robertc@robertcollins.net>
2020-07-01 13:36:47 +1200

    Fix homedir detection on Windows
    Correctly pass in the home::OS_ENV instance in non-test code.

--- src/currentprocess/homethunk.rs
+++ src/currentprocess/homethunk.rs
@@ -5,10 +5,24 @@ use std::ops::Deref;
 use std::path::PathBuf;
 
 use super::CurrentDirSource;
-use super::CurrentProcess;
+use super::HomeProcess;
+use super::OSProcess;
+use super::TestProcess;
 use super::VarSource;
 
-impl home::Env for Box<dyn CurrentProcess + 'static> {
+impl home::Env for Box<dyn HomeProcess + 'static> {
+    fn home_dir(&self) -> Option<PathBuf> {
+        (**self).home_dir()
+    }
+    fn current_dir(&self) -> Result<PathBuf, io::Error> {
+        home::Env::current_dir(&(**self))
+    }
+    fn var_os(&self, key: &str) -> Option<OsString> {
+        home::Env::var_os(&(**self), key)
+    }
+}
+
+impl home::Env for TestProcess {
     fn home_dir(&self) -> Option<PathBuf> {
         self.var("HOME").ok().map(|v| v.into())
     }
@@ -17,5 +31,17 @@ impl home::Env for Box<dyn CurrentProcess + 'static> {
     }
     fn var_os(&self, key: &str) -> Option<OsString> {
         VarSource::var_os(self.deref(), key)
+    }
+}
+
+impl home::Env for OSProcess {
+    fn home_dir(&self) -> Option<PathBuf> {
+        home::OS_ENV.home_dir()
+    }
+    fn current_dir(&self) -> Result<PathBuf, io::Error> {
+        home::OS_ENV.current_dir()
+    }
+    fn var_os(&self, key: &str) -> Option<OsString> {
+        home::OS_ENV.var_os(key)
     }
 }
