da957b2256f9508623f3a34d9bfc8a8ffdf1ce2a
Jubilee Young <workingjubilee@gmail.com>
2020-07-17 17:35:46 -0700

    Fix message to match script name

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -161,7 +161,7 @@ To get started you need Cargo's bin directory ({cargo_home}/bin) in your `PATH`
 environment variable. Next time you log in this will be done
 automatically.
 
-To configure your current shell run `source {cargo_home}/env`
+To configure your current shell run `source {cargo_home}/env.sh`
 "
     };
 }
@@ -184,7 +184,7 @@ macro_rules! post_install_msg_unix_no_modify_path {
 To get started you need Cargo's bin directory ({cargo_home}/bin) in your `PATH`
 environment variable.
 
-To configure your current shell run `source {cargo_home}/env`
+To configure your current shell run `source {cargo_home}/env.sh`
 "
     };
 }
