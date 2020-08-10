1ad2b413ccb2c859762132337e66fc5741ca704d
Jubilee Young <workingjubilee@gmail.com>
2020-08-09 21:56:06 -0700

    Simplify build_triple variable
    Also removes unnecessarily gendered language.

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -986,19 +986,16 @@ pub fn prepare_update() -> Result<Option<PathBuf>> {
     }
 
     // Get build triple
-    let build_triple = dist::TargetTriple::from_build();
-    let triple = if cfg!(windows) {
-        // For windows x86 builds seem slow when used with windows defender.
-        // The website defaulted to i686-windows-gnu builds for a long time.
-        // This ensures that we update to a version thats appropriate for users
-        // and also works around if the website messed up the detection.
-        // If someone really wants to use another version, he still can enforce
-        // that using the environment variable RUSTUP_OVERRIDE_HOST_TRIPLE.
+    let triple = dist::TargetTriple::from_build();
 
-        dist::TargetTriple::from_host().unwrap_or(build_triple)
-    } else {
-        build_triple
-    };
+    // For windows x86 builds seem slow when used with windows defender.
+    // The website defaulted to i686-windows-gnu builds for a long time.
+    // This ensures that we update to a version thats appropriate for users
+    // and also works around if the website messed up the detection.
+    // If someone really wants to use another version, they still can enforce
+    // that using the environment variable RUSTUP_OVERRIDE_HOST_TRIPLE.
+    #[cfg(windows)]
+    let triple = dist::TargetTriple::from_host().unwrap_or(triple);
 
     let update_root = process()
         .var("RUSTUP_UPDATE_ROOT")
