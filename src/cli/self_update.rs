1a7e6e2ec422700f446066c6bcb5d262e49c1ae3
Aurelia Dolo <aurelia.dolo@gmail.com>
2020-10-02 22:52:55 +0200

    Improve post installation sourcing instructions for readability.
    Co-authored-by: Joshua Nelson <joshua@yottadb.com>

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -166,7 +166,8 @@ To get started you need Cargo's bin directory ({cargo_home}/bin) in your `PATH`
 environment variable. Next time you log in this will be done
 automatically.
 
-To configure your current shell run `source {cargo_home}/env`
+To configure your current shell run :
+    source {cargo_home}/env
 "
     };
 }
@@ -191,7 +192,8 @@ macro_rules! post_install_msg_unix_no_modify_path {
 To get started you need Cargo's bin directory ({cargo_home}/bin) in your `PATH`
 environment variable.
 
-To configure your current shell run `source {cargo_home}/env`
+To configure your current shell run :
+    source {cargo_home}/env
 "
     };
 }
