ef4e933735e2b6f7ed1a090a937c6d909988f7a8
Daniel Silverstone <dsilvers@digital-scurf.org>
2020-11-17 13:47:31 +0000

    build: Use newly factored out triple.rs in build script
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- build.rs
+++ build.rs
@@ -1,6 +1,30 @@
 use std::env;
 
+include!("src/dist/triple.rs");
+
+pub fn from_build() -> Result<PartialTargetTriple, String> {
+    let triple = if let Ok(triple) = env::var("RUSTUP_OVERRIDE_BUILD_TRIPLE") {
+        triple
+    } else {
+        env::var("TARGET").unwrap()
+    };
+    PartialTargetTriple::new(&triple).ok_or(triple)
+}
+
 fn main() {
+    println!("cargo:rerun-if-env-changed=RUSTUP_OVERRIDE_BUILD_TRIPLE");
+    println!("cargo:rerun-if-env-changed=TARGET");
+    match from_build() {
+        Ok(triple) => eprintln!("Computed build based partial target triple: {:#?}", triple),
+        Err(s) => {
+            eprintln!("Unable to parse target '{}' as a PartialTargetTriple", s);
+            eprintln!(
+                "If you are attempting to bootstrap a new target you may need to adjust the\n\
+               permitted values found in src/dist/triple.rs"
+            );
+            std::process::abort();
+        }
+    }
     let target = env::var("TARGET").unwrap();
     println!("cargo:rustc-env=TARGET={}", target);
 }
