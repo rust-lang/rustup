3df2264a952174d7d18210bee78fdd0fdd7162cc
Daniel Silverstone <dsilvers@digital-scurf.org>
2020-11-29 20:52:54 +0000

    rustup-init.sh: Update for 1.23.1
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- rustup-init.sh
+++ rustup-init.sh
@@ -25,7 +25,7 @@ RUSTUP_UPDATE_ROOT="${RUSTUP_UPDATE_ROOT:-https://static.rust-lang.org/rustup}"
 #XXX: If you change anything here, please make the same changes in setup_mode.rs
 usage() {
     cat 1>&2 <<EOF
-rustup-init 1.23.0 (af3c77d66 2020-11-27)
+rustup-init 1.23.1 (fb4d10153 2020-12-01)
 The installer for rustup
 
 USAGE:
