5599a2ee15a425546dc24142f3e7c596b916a039
Robert Collins <robertc@robertcollins.net>
2023-05-30 22:43:20 +0200

    Make download_file async

--- src/cli/self_update/windows.rs
+++ src/cli/self_update/windows.rs
@@ -184,14 +184,19 @@ pub(crate) fn try_install_msvc(opts: &InstallOpts<'_>) -> Result<ContinueInstall
     download_tracker.lock().unwrap().download_finished();
 
     info!("downloading Visual Studio installer");
-    utils::download_file(&visual_studio_url, &visual_studio, None, &move |n| {
-        download_tracker
-            .lock()
-            .unwrap()
-            .handle_notification(&crate::Notification::Install(
-                crate::dist::Notification::Utils(n),
-            ));
-    })?;
+    utils::run_future(utils::download_file(
+        &visual_studio_url,
+        &visual_studio,
+        None,
+        &move |n| {
+            download_tracker
+                .lock()
+                .unwrap()
+                .handle_notification(&crate::Notification::Install(
+                    crate::dist::Notification::Utils(n),
+                ));
+        },
+    ))?;
 
     // Run the installer. Arguments are documented at:
     // https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
