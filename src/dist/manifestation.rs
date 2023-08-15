2b4cf3110b33ec02948b12ffebeb29059ede0f22
Robert Collins <robertc@robertcollins.net>
2023-08-15 23:52:38 +0200

    Make update_v1 async

--- src/dist/manifestation.rs
+++ src/dist/manifestation.rs
@@ -378,7 +378,7 @@ impl Manifestation {
     }
 
     /// Installation using the legacy v1 manifest format
-    pub(crate) fn update_v1(
+    pub(crate) async fn update_v1(
         &self,
         new_manifest: &[String],
         update_hash: Option<&Path>,
@@ -421,7 +421,9 @@ impl Manifestation {
             notify_handler,
         };
 
-        let dl = utils::run_future(dlcfg.download_and_check(&url, update_hash, ".tar.gz"))?;
+        let dl = dlcfg
+            .download_and_check(&url, update_hash, ".tar.gz")
+            .await?;
         if dl.is_none() {
             return Ok(None);
         };
