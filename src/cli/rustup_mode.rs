1a4cfe65ecc5a8be5c8397528380889084e90735
Brett Earle <brettearle@gmail.com>
2024-03-07 06:48:01 +1030

    http doc path WIP

--- src/cli/rustup_mode.rs
+++ src/cli/rustup_mode.rs
@@ -1616,33 +1616,27 @@ fn doc(cfg: &Cfg, m: &ArgMatches) -> Result<utils::ExitCode> {
         let doc_path_index = toolchain.doc_path(doc_url)?;
         let doc_path_base = toolchain.doc_path("")?;
         let doc_path_str = doc_path_base.to_string_lossy().into_owned();
-        println!("serving doc from {}", doc_path_base.display());
         loop {
             let server = Server::http("127.0.0.1:3000").unwrap();
             for request in server.incoming_requests() {
                 //TODO get request path and serve filebased on that
-                let request_path = request.url();
-                println!("request path: {}", request_path);
+                let request_path = request.url().strip_prefix('/');
+                println!("request path: {:?}", request_path);
                 let mut response_path =
                     doc_path_base.clone().to_string_lossy().into_owned() + request_path;
-                if request_path == "/" {
+                println!("response path: {}", response_path);
+                if request_path == Option::from("/") {
                     response_path = doc_path_index.clone().to_string_lossy().into_owned();
                 }
-                let request_path_from_doc = doc_path_str.clone() + request_path;
+                let request_path_from_doc = doc_path_str.clone() + request_path.strip_prefix('/');
                 println!("request path: {}", request_path_from_doc);
                 let file = std::fs::File::open(&response_path).unwrap();
-                println!(
-                    "received request! method: {:?}, url: {:?}, headers: {:?}",
-                    request.method(),
-                    request.url(),
-                    request.headers()
-                );
+                println!("received request! url: {:?}", request.url(),);
 
                 let response = Response::from_file(file);
-                request.respond(response);
+                request.respond(response).unwrap();
             }
         }
-        Ok(utils::ExitCode(0))
     } else {
         toolchain.open_docs(doc_url)?;
         Ok(utils::ExitCode(0))
