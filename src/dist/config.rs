2070bb7a42ac0635133f4d6855aecd8acebae54a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-06-06 15:12:52 +0200

    Use serde to encode/decode config

--- src/dist/config.rs
+++ src/dist/config.rs
@@ -2,72 +2,24 @@ use std::fmt;
 use std::str::FromStr;
 
 use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
 
 use super::manifest::Component;
 use crate::errors::*;
-use crate::utils::toml_utils::*;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Config {
     pub config_version: ConfigVersion,
     pub components: Vec<Component>,
 }
 
 impl Config {
-    pub(crate) fn from_toml(mut table: toml::value::Table, path: &str) -> Result<Self> {
-        let config_version = get_string(&mut table, "config_version", path)?;
-        let config_version = ConfigVersion::from_str(&config_version)?;
-
-        let components = get_array(&mut table, "components", path)?;
-        let components =
-            Self::toml_to_components(components, &format!("{}{}.", path, "components"))?;
-
-        Ok(Self {
-            config_version,
-            components,
-        })
-    }
-    pub(crate) fn into_toml(self) -> toml::value::Table {
-        let mut result = toml::value::Table::new();
-        result.insert(
-            "config_version".to_owned(),
-            toml::Value::String(self.config_version.as_str().to_owned()),
-        );
-        let components = Self::components_to_toml(self.components);
-        if !components.is_empty() {
-            result.insert("components".to_owned(), toml::Value::Array(components));
-        }
-        result
-    }
-
     pub(crate) fn parse(data: &str) -> Result<Self> {
-        let value = toml::from_str(data).context("error parsing config")?;
-        Self::from_toml(value, "")
+        toml::from_str(data).context("error parsing config")
     }
 
-    pub(crate) fn stringify(self) -> String {
-        self.into_toml().to_string()
-    }
-
-    fn toml_to_components(arr: toml::value::Array, path: &str) -> Result<Vec<Component>> {
-        let mut result = Vec::new();
-
-        for (i, v) in arr.into_iter().enumerate() {
-            if let toml::Value::Table(t) = v {
-                let path = format!("{path}[{i}]");
-                result.push(Component::from_toml(t, &path, false)?);
-            }
-        }
-
-        Ok(result)
-    }
-
-    fn components_to_toml(components: Vec<Component>) -> toml::value::Array {
-        let mut result = toml::value::Array::new();
-        for v in components {
-            result.push(toml::Value::Table(v.into_toml()));
-        }
-        result
+    pub(crate) fn stringify(&self) -> Result<String> {
+        Ok(toml::to_string(&self)?)
     }
 
     pub(crate) fn new() -> Self {
@@ -75,8 +27,9 @@ impl Config {
     }
 }
 
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub(crate) enum ConfigVersion {
+    #[serde(rename = "1")]
     #[default]
     V1,
 }
