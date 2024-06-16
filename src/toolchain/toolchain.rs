f88394cbbbcc25ffff8e0a0fd465ae77ef22ccbf
Rean Fei <thebluealgae1123@outlook.com>
2024-06-17 00:20:13 +0800

    Fix the `TODO` in `src\toolchain\toolchain.rs`

--- src/toolchain/toolchain.rs
+++ src/toolchain/toolchain.rs
@@ -278,50 +278,48 @@ impl<'a> Toolchain<'a> {
     /// Infallible function that describes the version of rustc in an installed distribution
     #[cfg_attr(feature = "otel", tracing::instrument)]
     pub fn rustc_version(&self) -> String {
-        // TODO: use create_command instead of manual construction!
-        let rustc_path = self.binary_file("rustc");
-        if utils::is_file(&rustc_path) {
-            let mut cmd = Command::new(&rustc_path);
-            cmd.arg("--version");
-            cmd.stdin(Stdio::null());
-            cmd.stdout(Stdio::piped());
-            cmd.stderr(Stdio::piped());
-            self.set_ldpath(&mut cmd);
+        match self.create_command("rustc") {
+            Ok(mut cmd) => {
+                cmd.arg("--version");
+                cmd.stdin(Stdio::null());
+                cmd.stdout(Stdio::piped());
+                cmd.stderr(Stdio::piped());
+                self.set_ldpath(&mut cmd);
 
-            // some toolchains are faulty with some combinations of platforms and
-            // may fail to launch but also to timely terminate.
-            // (known cases include Rust 1.3.0 through 1.10.0 in recent macOS Sierra.)
-            // we guard against such cases by enforcing a reasonable timeout to read.
-            let mut line1 = None;
-            if let Ok(mut child) = cmd.spawn() {
-                let timeout = Duration::new(10, 0);
-                match child.wait_timeout(timeout) {
-                    Ok(Some(status)) if status.success() => {
-                        let out = child
-                            .stdout
-                            .expect("Child::stdout requested but not present");
-                        let mut line = String::new();
-                        if BufReader::new(out).read_line(&mut line).is_ok() {
-                            let lineend = line.trim_end_matches(&['\r', '\n'][..]).len();
-                            line.truncate(lineend);
-                            line1 = Some(line);
+                // some toolchains are faulty with some combinations of platforms and
+                // may fail to launch but also to timely terminate.
+                // (known cases include Rust 1.3.0 through 1.10.0 in recent macOS Sierra.)
+                // we guard against such cases by enforcing a reasonable timeout to read.
+                let mut line1 = None;
+                if let Ok(mut child) = cmd.spawn() {
+                    let timeout = Duration::new(10, 0);
+                    match child.wait_timeout(timeout) {
+                        Ok(Some(status)) if status.success() => {
+                            let out = child
+                                .stdout
+                                .expect("Child::stdout requested but not present");
+                            let mut line = String::new();
+                            if BufReader::new(out).read_line(&mut line).is_ok() {
+                                let lineend = line.trim_end_matches(&['\r', '\n'][..]).len();
+                                line.truncate(lineend);
+                                line1 = Some(line);
+                            }
                         }
+                        Ok(None) => {
+                            let _ = child.kill();
+                            return String::from("(timeout reading rustc version)");
+                        }
+                        Ok(Some(_)) | Err(_) => {}
                     }
-                    Ok(None) => {
-                        let _ = child.kill();
-                        return String::from("(timeout reading rustc version)");
-                    }
-                    Ok(Some(_)) | Err(_) => {}
+                }
+
+                if let Some(line1) = line1 {
+                    line1
+                } else {
+                    String::from("(error reading rustc version)")
                 }
             }
-
-            if let Some(line1) = line1 {
-                line1
-            } else {
-                String::from("(error reading rustc version)")
-            }
-        } else {
-            String::from("(rustc does not exist)")
+            Err(_) => String::from("(rustc does not exist)"),
         }
     }
 
