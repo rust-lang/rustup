7486b52f91e05295e029d17b26d1758d83d0ffb6
rami3l <rami3l@outlook.com>
2025-10-13 22:13:12 +0800

    revert(ci): exclude aarch64-pc-windows-gnullvm from aws-lc-rs
    This reverts commit 4c1876cabf8f414377a97f934939634fc3fcadfa.

--- ci/run.bash
+++ ci/run.bash
@@ -34,7 +34,6 @@ case "$TARGET" in
   *netbsd* ) ;;
   *illumos* ) ;;
   *solaris* ) ;;
-  aarch64-pc-windows-gnullvm ) ;;
   # default case, build with rustls enabled
   * ) FEATURES+=('--features' 'reqwest-rustls-tls') ;;
 esac
@@ -68,7 +67,6 @@ build_test() {
     mips* ) ;;
     riscv* ) ;;
     s390x* ) ;;
-    aarch64-pc-windows-gnullvm ) ;;
     # default case, build with rustls enabled
     * ) features+=('--features' 'reqwest-rustls-tls') ;;
   esac
