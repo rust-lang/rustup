f582d5e105f62c9d8da0fb42054c39b18077f22a
Tshepang Mbambo <hopsi@tuta.io>
2026-01-01 15:30:57 +0200

    rename triple module to tuple

--- src/dist/mod.rs
+++ src/dist/mod.rs
@@ -44,8 +44,8 @@ use prefix::InstallPrefix;
 
 pub mod temp;
 
-pub(crate) mod triple;
-pub(crate) use triple::*;
+pub(crate) mod tuple;
+pub(crate) use tuple::*;
 
 pub static DEFAULT_DIST_SERVER: &str = "https://static.rust-lang.org";
 
