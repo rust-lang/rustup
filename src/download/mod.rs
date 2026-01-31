1c27a701abe0a262b88c41adfa08ffd2f82f2b70
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2026-01-31 13:34:23 +0000

    fix(downloads): do not delete partial download when network fails

--- src/download/mod.rs
+++ src/download/mod.rs
@@ -21,7 +21,10 @@ use tracing::info;
 use tracing::warn;
 use url::Url;
 
-use crate::{dist::download::DownloadStatus, errors::RustupError, process::Process};
+use crate::{
+    dist::download::DownloadStatus, download::DownloadError as DEK, errors::RustupError,
+    process::Process,
+};
 
 #[cfg(test)]
 mod tests;
@@ -44,7 +47,6 @@ pub(crate) async fn download_file_with_resume(
     status: Option<&DownloadStatus>,
     process: &Process,
 ) -> anyhow::Result<()> {
-    use crate::download::DownloadError as DEK;
     match download_file_(url, path, hasher, resume_from_partial, status, process).await {
         Ok(_) => Ok(()),
         Err(e) => {
@@ -261,9 +263,16 @@ impl Backend {
             return Ok(());
         };
 
+        let is_network_failure = match err.downcast_ref::<DEK>() {
+            Some(DEK::Reqwest(e)) => e.is_timeout() || e.is_connect(),
+            _ => false,
+        };
+
         // TODO: We currently clear up the cached download on any error, should we restrict it to a subset?
         Err(
-            if let Err(file_err) = remove_file(path).context("cleaning up cached downloads") {
+            if !(resume_from_partial && is_network_failure)
+                && let Err(file_err) = remove_file(path).context("cleaning up cached downloads")
+            {
                 file_err.context(err)
             } else {
                 err
@@ -586,7 +595,7 @@ mod reqwest_be {
 
         let mut stream = res.bytes_stream();
         while let Some(item) = stream.next().await {
-            let bytes = item?;
+            let bytes = item.map_err(DownloadError::Reqwest)?;
             callback(Event::DownloadDataReceived(&bytes))?;
         }
         Ok(())
