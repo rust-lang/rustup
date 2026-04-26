6b81debace915ad0ca59e97eee3083f49b901558
motorailgun <28751910+motorailgun@users.noreply.github.com>
2026-04-26 18:26:44 +0000

    refactor(dist): rename PartialTargetTriple to PartialTargetTuple

--- src/dist/target_tuple.rs
+++ src/dist/target_tuple.rs
@@ -5,13 +5,13 @@ use regex::Regex;
 pub mod known;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct PartialTargetTriple {
+pub struct PartialTargetTuple {
     pub arch: Option<String>,
     pub os: Option<String>,
     pub env: Option<String>,
 }
 
-impl PartialTargetTriple {
+impl PartialTargetTuple {
     pub(crate) fn new(name: &str) -> Option<Self> {
         if name.is_empty() {
             return Some(Self {
@@ -22,7 +22,7 @@ impl PartialTargetTriple {
         }
 
         // Prepending `-` makes this next regex easier since
-        // we can count  on all triple components being
+        // we can count on all tuple components being
         // delineated by it.
         let name = format!("-{name}");
         static RE: LazyLock<Regex> = LazyLock::new(|| {
@@ -62,7 +62,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_partial_target_triple_new() {
+    fn test_partial_target_tuple_new() {
         let success_cases = vec![
             ("", (None, None, None)),
             ("i386", (Some("i386"), None, None)),
@@ -78,19 +78,19 @@ mod test {
         ];
 
         for (input, (arch, os, env)) in success_cases {
-            let partial_target_triple = PartialTargetTriple::new(input);
+            let partial_target_tuple = PartialTargetTuple::new(input);
             assert!(
-                partial_target_triple.is_some(),
+                partial_target_tuple.is_some(),
                 "expected `{input}` to create some partial target tuple; got None"
             );
 
-            let expected = PartialTargetTriple {
+            let expected = PartialTargetTuple {
                 arch: arch.map(String::from),
                 os: os.map(String::from),
                 env: env.map(String::from),
             };
 
-            assert_eq!(partial_target_triple.unwrap(), expected, "input: `{input}`");
+            assert_eq!(partial_target_tuple.unwrap(), expected, "input: `{input}`");
         }
 
         let failure_cases = vec![
@@ -108,10 +108,10 @@ mod test {
         ];
 
         for input in failure_cases {
-            let partial_target_triple = PartialTargetTriple::new(input);
+            let partial_target_tuple = PartialTargetTuple::new(input);
             assert!(
-                partial_target_triple.is_none(),
-                "expected `{input}` to be `None`, was: `{partial_target_triple:?}`"
+                partial_target_tuple.is_none(),
+                "expected `{input}` to be `None`, was: `{partial_target_tuple:?}`"
             );
         }
     }
