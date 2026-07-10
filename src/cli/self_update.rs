414cd98c1ada6d04259ce0a6d31ffebdeb439571
rami3l <rami3l@outlook.com>
2026-07-10 16:31:13 +0200

    refactor: replace `cfg_if!{}` with `cfg_select!{}`

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -42,7 +42,6 @@ use std::{fmt, fs};
 
 use anstyle::Style;
 use anyhow::{Context, Result, anyhow};
-use cfg_if::cfg_if;
 use clap::ValueEnum;
 use clap::builder::PossibleValue;
 use clap_cargo::style::{GOOD, WARN};
@@ -539,12 +538,9 @@ fn canonical_cargo_home(process: &Process) -> Result<Cow<'static, str>> {
         .unwrap_or_else(|| PathBuf::from("."))
         .join(".cargo");
     Ok(if default_cargo_home == path {
-        cfg_if! {
-            if #[cfg(windows)] {
-                r"%USERPROFILE%\.cargo".into()
-            } else {
-                "$HOME/.cargo".into()
-            }
+        cfg_select! {
+            windows => r"%USERPROFILE%\.cargo".into(),
+            _ => "$HOME/.cargo".into(),
         }
     } else {
         path.to_string_lossy().into_owned().into()
