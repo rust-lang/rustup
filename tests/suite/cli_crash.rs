7e670fa70723fce0b3a323e97ee74947a9540603
rami3l <rami3l@outlook.com>
2026-07-20 10:43:37 +0200

    refactor(test/clitools)!: refine semantics of `CliTestContext::kill_at()`
    Co-authored-by: akrm al-hakimi <alhakimiakrmj@gmail.com>

--- tests/suite/cli_crash.rs
+++ tests/suite/cli_crash.rs
@@ -3,9 +3,10 @@ use rustup::test::{CliTestContext, Scenario};
 #[tokio::test]
 async fn interrupted_install_can_be_retried() {
     let cx = CliTestContext::new(Scenario::SimpleV2).await;
-    let command = cx.config.cmd("rustup", ["toolchain", "install", "nightly"]);
-
-    let status = cx.kill_at_checkpoint(command, "manifestation-update-before-metadata");
+    let status = cx.kill_at(
+        "manifestation-update-before-metadata",
+        ["rustup", "toolchain", "install", "nightly"],
+    );
     assert!(!status.success());
 
     cx.config
