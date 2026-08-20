8db86b56ed3ff945d0c22a302bbfef59bf3f61af
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2026-08-20 21:29:57 +0800

    feat(config): expose rustup config home through Cfg

--- src/config.rs
+++ src/config.rs
@@ -320,6 +320,7 @@ pub(crate) struct Cfg<'a> {
     fallback_settings: Option<FallbackSettings>,
     pub toolchains_dir: PathBuf,
     pub rustup_cache_dir: PathBuf,
+    pub rustup_config_dir: PathBuf,
     pub download_dir: PathBuf,
     pub toolchain_override: Option<ResolvableToolchainName>,
     env_override: Option<ResolvableLocalToolchainName>,
@@ -347,7 +348,9 @@ impl<'a> Cfg<'a> {
     ) -> anyhow::Result<Self> {
         // Set up the rustup home directory
         let rustup_dir = process.rustup_home()?;
-        let rustup_cache_dir = process.home_dirs()?.cache;
+        let home_dirs = process.home_dirs()?;
+        let rustup_cache_dir = home_dirs.cache;
+        let rustup_config_dir = home_dirs.config;
 
         utils::ensure_dir_exists("home", &rustup_dir)?;
 
@@ -398,6 +401,7 @@ impl<'a> Cfg<'a> {
             fallback_settings,
             toolchains_dir,
             rustup_cache_dir,
+            rustup_config_dir,
             download_dir,
             toolchain_override: None,
             env_override,
@@ -1184,6 +1188,7 @@ impl Debug for Cfg<'_> {
             fallback_settings,
             toolchains_dir,
             rustup_cache_dir,
+            rustup_config_dir,
             download_dir,
             toolchain_override,
             env_override,
@@ -1203,6 +1208,7 @@ impl Debug for Cfg<'_> {
             .field("fallback_settings", fallback_settings)
             .field("toolchains_dir", toolchains_dir)
             .field("rustup_cache_dir", rustup_cache_dir)
+            .field("rustup_config_dir", rustup_config_dir)
             .field("download_dir", download_dir)
             .field("toolchain_override", toolchain_override)
             .field("env_override", env_override)
