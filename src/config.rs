b70af7d871a8197a967295c5f349650c41fdce0c
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2026-08-20 21:30:13 +0800

    feat(config): expose rustup data home through Cfg

--- src/config.rs
+++ src/config.rs
@@ -321,6 +321,7 @@ pub(crate) struct Cfg<'a> {
     pub toolchains_dir: PathBuf,
     pub rustup_cache_dir: PathBuf,
     pub rustup_config_dir: PathBuf,
+    pub rustup_data_dir: PathBuf,
     pub download_dir: PathBuf,
     pub toolchain_override: Option<ResolvableToolchainName>,
     env_override: Option<ResolvableLocalToolchainName>,
@@ -351,6 +352,7 @@ impl<'a> Cfg<'a> {
         let home_dirs = process.home_dirs()?;
         let rustup_cache_dir = home_dirs.cache;
         let rustup_config_dir = home_dirs.config;
+        let rustup_data_dir = home_dirs.data;
 
         utils::ensure_dir_exists("home", &rustup_dir)?;
         utils::ensure_dir_exists("config home", &rustup_config_dir)?;
@@ -403,6 +405,7 @@ impl<'a> Cfg<'a> {
             toolchains_dir,
             rustup_cache_dir,
             rustup_config_dir,
+            rustup_data_dir,
             download_dir,
             toolchain_override: None,
             env_override,
@@ -1190,6 +1193,7 @@ impl Debug for Cfg<'_> {
             toolchains_dir,
             rustup_cache_dir,
             rustup_config_dir,
+            rustup_data_dir,
             download_dir,
             toolchain_override,
             env_override,
@@ -1210,6 +1214,7 @@ impl Debug for Cfg<'_> {
             .field("toolchains_dir", toolchains_dir)
             .field("rustup_cache_dir", rustup_cache_dir)
             .field("rustup_config_dir", rustup_config_dir)
+            .field("rustup_data_dir", rustup_data_dir)
             .field("download_dir", download_dir)
             .field("toolchain_override", toolchain_override)
             .field("env_override", env_override)
