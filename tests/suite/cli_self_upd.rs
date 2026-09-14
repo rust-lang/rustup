e3c6fc3b14813048bf24705b265c91257d056e6c
Akrm Al-Hakimi <akrm@cachebag.sh>
2026-09-14 08:34:27 -0600

    style(self-update): move test constant

--- tests/suite/cli_self_upd.rs
+++ tests/suite/cli_self_upd.rs
@@ -501,12 +501,6 @@ async fn update_overwrites_programs_display_version() {
     );
 }
 
-#[cfg(windows)]
-const USER_RUSTUP_VERSION: RegistryValueId = RegistryValueId {
-    sub_key: r"Software\Microsoft\Windows\CurrentVersion\Uninstall\Rustup",
-    value_name: "DisplayVersion",
-};
-
 #[tokio::test]
 async fn update_but_not_installed() {
     let cx = SelfUpdateTestContext::new(TEST_VERSION).await;
@@ -1243,3 +1237,8 @@ async fn install_minimal_profile() {
 }
 
 const TEST_VERSION: &str = "1.1.1";
+#[cfg(windows)]
+const USER_RUSTUP_VERSION: RegistryValueId = RegistryValueId {
+    sub_key: r"Software\Microsoft\Windows\CurrentVersion\Uninstall\Rustup",
+    value_name: "DisplayVersion",
+};
