fee254dc4a0a0698ccf481a26b7f282b0e262e17
rami3l <rami3l@outlook.com>
2026-09-17 17:34:12 +0200

    fix(dist): add `DistError::HostTupleUnsupported` to nightly backtracking flow

--- tests/suite/cli_rustup.rs
+++ tests/suite/cli_rustup.rs
@@ -4114,12 +4114,12 @@ async fn check_host_goes_away() {
         .await
         .with_stderr(snapbox::str![[r#"
 ...
-error: host tuple '[HOST_TUPLE]' not found in channel manifest
+info: skipping nightly with unsupported host: host tuple '[HOST_TUPLE]' not found in channel manifest
 help: this could mean that it has been demoted and no longer has a host toolchain
 help: see <https://doc.rust-lang.org/nightly/rustc/platform-support.html> for available targets
 ...
 "#]])
-        .is_err();
+        .is_ok();
 }
 
 #[cfg(unix)]
